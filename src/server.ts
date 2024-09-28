d80e84a8d0332e598b19bb35deaba95abe517d2f
FelipeStemp <felipestempkowski17@gmail.com>
2024-09-28 12:54:14 -0300

    att cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+import cors from 'cors'; // Importando o cors
 import express from 'express';
-const mongoose = require("mongoose");
-import routes from "./routes/routes"
+import mongoose from 'mongoose';
+import routes from "./routes/routes";
 
 require("dotenv").config();
 
@@ -9,10 +10,18 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(MONGO_URL);
 
 const app = express();
-app.use(express.json())
 
-app.use('/', routes)
+// Configurando o CORS
+app.use(cors({
+    origin: 'http://localhost:3000', // Permite requisições apenas do seu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type'], // Cabeçalhos permitidos
+}));
 
-app.listen(3000, ()=> {
-    console.log("Server running on http://localhost:3000/")
-})
+app.use(express.json());
+
+app.use('/', routes);
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000/");
+});
