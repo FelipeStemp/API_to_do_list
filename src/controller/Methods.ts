68906d23e09e8e6b986a46ff6d3faaea9c5edc92
Felipe Stempkowski <97076665+FelipeStemp@users.noreply.github.com>
2024-10-02 09:53:28 -0300

    Update Methods.ts

--- src/controller/Methods.ts
+++ src/controller/Methods.ts
@@ -48,7 +48,7 @@ export const getItemByIdOrName = async (Request: express.Request , Response: exp
         const id = Request.params.id || null;
         const name = Request.params.name || null;
 
-        const existItem = await getItemNameID(name.toLowerCase() : null, id);
+        const existItem = await getItemNameID(name.toLowerCase(), id);
 
         if(!existItem){
             return Response.status(404).json({error: "Item not found"})
